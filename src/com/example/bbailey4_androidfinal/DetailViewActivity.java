474b3f5f66085a20a3cf93915354bf7c0022f9aa
Brian T Bailey <briantbailey@gmail.com>
2013-02-26 20:56:48 -0600

    Finished Detail Activity Layout

--- src/com/example/bbailey4_androidfinal/DetailViewActivity.java
+++ src/com/example/bbailey4_androidfinal/DetailViewActivity.java
@@ -18,8 +18,34 @@ public class DetailViewActivity extends Activity {
 		Bundle b = thisIntent.getExtras();
 		
 		// Populate Views
-		TextView tv = (TextView) findViewById(R.id.tvDetailTest);
-		tv.setText(b.getString("block"));
+		TextView tvCase = (TextView) findViewById(R.id.tvDetailCaseNo);
+		tvCase.setText(b.getString("caseNum"));
+		TextView tvDate = (TextView) findViewById(R.id.tvDetailDateOf);
+		tvDate.setText((b.getString("dateOf")).substring(0, 10));
+		TextView tvBlock = (TextView) findViewById(R.id.tvDetailBlock);
+		tvBlock.setText((b.getString("block")));
+		TextView tvWard = (TextView) findViewById(R.id.tvDetailWard);
+		tvWard.setText((b.getString("ward")));
+		TextView tvLat = (TextView) findViewById(R.id.tvDetailLat);
+		tvLat.setText((b.getString("latitude")));
+		TextView tvLong = (TextView) findViewById(R.id.tvDetailLong);
+		tvLong.setText((b.getString("longitude")));
+		TextView tvLoc = (TextView) findViewById(R.id.tvDetailLocation);
+		tvLoc.setText((b.getString("locationDesc")));
+		TextView tvPrimary = (TextView) findViewById(R.id.tvDetailPrimary);
+		tvPrimary.setText((b.getString("primaryDesc")));
+		TextView tvSecondary = (TextView) findViewById(R.id.tvDetailSecondary);
+		tvSecondary.setText((b.getString("secondaryDesc")));
+		TextView tvBeat = (TextView) findViewById(R.id.tvDetailBeat);
+		tvBeat.setText((b.getString("beat")));
+		TextView tvDom = (TextView) findViewById(R.id.tvDetailDomestic);
+		tvDom.setText((b.getString("domestic")));
+		TextView tvArrest = (TextView) findViewById(R.id.tvDetailArrest);
+		tvArrest.setText((b.getString("arrest")));
+		TextView tvIucr = (TextView) findViewById(R.id.tvDetailIucr);
+		tvIucr.setText((b.getString("iucr")));
+		TextView tvFbi = (TextView) findViewById(R.id.tvDetailFBI);
+		tvFbi.setText((b.getString("fbi_cd")));
 	}
 
 	@Override
